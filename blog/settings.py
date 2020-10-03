47f61161f38b67339cf072cc2eed3f12cd2e34f5
ishanExtreme <ishan2198@hotmail.com>
2020-10-03 22:09:45 +0530

    v1.5

--- blog/settings.py
+++ blog/settings.py
@@ -303,7 +303,7 @@ DEFAULT_FILE_STORAGE = 'blogs.custom_azure.AzureMediaStorage'
 # STATIC_LOCATION = "static"
 MEDIA_LOCATION = "media"
 
-AZURE_ACCOUNT_NAME = "udaanstorage123"
+AZURE_ACCOUNT_NAME = os.environ.get('Azure_Account_Name', '')
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
 # STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
 MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
