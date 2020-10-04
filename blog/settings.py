2325a6421d27b6793d8eedb63668f3388a9ee30c
ishanExtreme <ishan2198@hotmail.com>
2020-10-04 05:30:15 +0530

    v2.0

--- blog/settings.py
+++ blog/settings.py
@@ -67,50 +67,50 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'blog.urls'
 
 
-# servers = os.environ['MEMCACHIER_SERVERS']
-# username = os.environ['MEMCACHIER_USERNAME']
-# password = os.environ['MEMCACHIER_PASSWORD']
+servers = os.environ['MEMCACHIER_SERVERS']
+username = os.environ['MEMCACHIER_USERNAME']
+password = os.environ['MEMCACHIER_PASSWORD']
 
-# CACHES = {
-#     'default': {
-#         # Use pylibmc
-#         'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+CACHES = {
+    'default': {
+        # Use pylibmc
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
 
-#         # TIMEOUT is not the connection timeout! It's the default expiration
-#         # timeout that should be applied to keys! Setting it to `None`
-#         # disables expiration.
-#         'TIMEOUT': None,
+        # TIMEOUT is not the connection timeout! It's the default expiration
+        # timeout that should be applied to keys! Setting it to `None`
+        # disables expiration.
+        'TIMEOUT': None,
 
-#         'LOCATION': servers,
+        'LOCATION': servers,
 
-#         'OPTIONS': {
-#             # Use binary memcache protocol (needed for authentication)
-#             'binary': True,
-#             'username': username,
-#             'password': password,
-#             'behaviors': {
-#                 # Enable faster IO
-#                 'no_block': True,
-#                 'tcp_nodelay': True,
+        'OPTIONS': {
+            # Use binary memcache protocol (needed for authentication)
+            'binary': True,
+            'username': username,
+            'password': password,
+            'behaviors': {
+                # Enable faster IO
+                'no_block': True,
+                'tcp_nodelay': True,
 
-#                 # Keep connection alive
-#                 'tcp_keepalive': True,
+                # Keep connection alive
+                'tcp_keepalive': True,
 
-#                 # Timeout settings
-#                 'connect_timeout': 2000, # ms
-#                 'send_timeout': 750 * 1000, # us
-#                 'receive_timeout': 750 * 1000, # us
-#                 '_poll_timeout': 2000, # ms
+                # Timeout settings
+                'connect_timeout': 2000, # ms
+                'send_timeout': 750 * 1000, # us
+                'receive_timeout': 750 * 1000, # us
+                '_poll_timeout': 2000, # ms
 
-#                 # Better failover
-#                 'ketama': True,
-#                 'remove_failed': 1,
-#                 'retry_timeout': 2,
-#                 'dead_timeout': 30,
-#             }
-#         }
-#     }
-# }
+                # Better failover
+                'ketama': True,
+                'remove_failed': 1,
+                'retry_timeout': 2,
+                'dead_timeout': 30,
+            }
+        }
+    }
+}
 
 USER_AGENTS_CACHE = None
 
